54246893b6e2820f696309b62c15b7cc13a48e8d
Zack Cain <zacharylcain@gmail.com>
2024-03-07 19:06:13 -0600

    changes to code to align better

--- scriptHelper.js
+++ scriptHelper.js
@@ -4,18 +4,18 @@ require('cross-fetch/polyfill');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
     // Here is the HTML formatting for our mission target div.
-    /*
+    return `
                  <h2>Mission Destination</h2>
                  <ol>
-                     <li>Name: </li>
-                     <li>Diameter: </li>
+                     <li>Name:${name} </li>
+                     <li>Diameter:${diameter} </li>
                      <li>Star: ${star}</li>
-                     <li>Distance from Earth: </li>
-                     <li>Number of Moons: </li>
+                     <li>Distance from Earth:${distance} </li>
+                     <li>Number of Moons:${moons} </li>
                  </ol>
-                 <img src="">
-    */
- }
+                 <img src="${imageUrl}">
+    `
+ };
  
  function validateInput(input) {
     if (input === "") {
@@ -28,14 +28,24 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
   }
  
  function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-    if ( isNaN(document, list, pilot, copilot, fuelLevel, cargoLevel )){
-        return('Not a Number')
-    } else  {
-    return('input is a Number')
-    }
-   
-    };
- 
+  // validateInput //
+  if(fuelLevel < 10000){
+      list.document.getElementById('faultyItems').style.display = 'block';
+      document.getElementById('fuelStatus').textContent = 'Not enough fuel for the Journey';
+      document.getElementById('launchStatus').textContent = 'Shuttle not ready for launch';
+      document.getElementById('launchStatus').style.color = 'red';
+  } else if (cargoLevel > 10000) {
+      list.document.getElementById('faultyItems').style.display = 'block';
+      document.getElementById('cargoStatus').textContent = alert('To much mass for the shuttle to take off');
+      document.getElementById('launchStatus').textContent = alert('Shuttle not ready for launch');
+      document.getElementById('launchStatus').style.color = 'red';
+  } else {
+      document.getElementById('lanchStatus').textContent = alert('Shuttle is ready for launch');
+      document.getElementById('launchStatus').style.color = 'green';
+      
+}
+
+};
  
  
  async function myFetch() {
@@ -47,7 +57,10 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
      return planetsReturned;
  }
  
- function pickPlanet(planets) {
+ function pickPlanet(planets) { // this was changed //
+  let planet = {};
+for(planet of planets)
+return planet.planets.
  }
  
  module.exports.addDestinationInfo = addDestinationInfo;
