94975ff9dbe6d5cb92595bb284073aa7fd6d4ba0
Zack Cain <zacharylcain@gmail.com>
2024-03-18 18:50:08 -0500

    Image loads

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Write your JavaScript code here!
 
-const { myFetch, pickPlanet, addDestinationInfo, formSubmission } = require("./scriptHelper");
+// const { myFetch, pickPlanet, addDestinationInfo, formSubmission } = require("./scriptHelper");
 
 window.addEventListener("load", function() {
         // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-
+        formSubmission();
     let listedPlanets; // need to store in an object //
     let listedPlanetsResponse = myFetch(); // this validates the above req. //
     listedPlanetsResponse.then(function (result) {
@@ -13,9 +13,9 @@ window.addEventListener("load", function() {
     }).then(function () {
         const randomPlanet = pickPlanet(listedPlanets);
         // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-        addDestinationInfo(document, randomPlanet.name, randomPlanet.diameter,randomPlanet.star, randomPlanet.distance, randomPlanet.moons, randomPlanet.imageUrl);
-        formSubmission();
-    })
+        addDestinationInfo(document, randomPlanet.name, randomPlanet.diameter,randomPlanet.star, randomPlanet.distance, randomPlanet.moons, randomPlanet.image);
     
+    })
+   
  });
  // eventlistner and event.prevent() 
