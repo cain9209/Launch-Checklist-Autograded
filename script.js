af38cede42b3517ddfb8776b588379b6a603c74d
Zack Cain <zacharylcain@gmail.com>
2024-03-19 06:23:48 -0500

    changes to allow more readability

--- script.js
+++ script.js
@@ -4,28 +4,28 @@
 
 // const { myFetch, pickPlanet, addDestinationInfo, formSubmission } = require("./scriptHelper");
 
-window.addEventListener("load", function() {
-        // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanets; // need to store in an object //
+window.addEventListener("load", function () {
+    let listedPlanets = {}; // need to store in an object //
     let listedPlanetsResponse = myFetch(); // this validates the above req. //
     listedPlanetsResponse.then(function (result) {
         listedPlanets = result;
         console.log(listedPlanets);
     }).then(function () {
         const randomPlanet = pickPlanet(listedPlanets);
-        
+
         // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-        addDestinationInfo(document, randomPlanet.name, randomPlanet.diameter,randomPlanet.star, randomPlanet.distance, randomPlanet.moons, randomPlanet.image);
-    
+        addDestinationInfo(document, randomPlanet.name, randomPlanet.diameter, randomPlanet.star, randomPlanet.distance, randomPlanet.moons, randomPlanet.image);
+
     })
-   
- document.addEventListener("submit", function(event) {
+
+    document.addEventListener("submit", function (event) {
         event.preventDefault();
-  const faultyItems =  document.querySelector('faultyItems')
-  const pilotName = document.querySelector('input[name=pilot]').value
-  const copilotName = document.querySelector('input[name=copilot]').value
-  const fuelLevel = Number(document.querySelector('input[name=fuelLevel]').value)
-  const cargoMass = Number(document.querySelector('input[name=cargoMass]').value)
-});
-formSubmission(document,faultyItems,pilotName,copilotName,fuelLevel,cargoMass)
+        formSubmission(document, faultyItems, pilotName, copilotName, fuelLevel, cargoMass)
+
+        const faultyItems = document.querySelector('faultyItems')
+        const pilotName = document.querySelector('input[name=pilot]').value
+        const copilotName = document.querySelector('input[name=copilot]').value
+        const fuelLevel = Number(document.querySelector('input[name=fuelLevel]').value)
+        const cargoMass = Number(document.querySelector('input[name=cargoMass]').value)
+    });
 });
