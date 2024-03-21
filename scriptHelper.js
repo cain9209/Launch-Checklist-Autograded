71a79b4b6e3479d769f7c8df894ff902acdd13d2
Zack Cain <zacharylcain@gmail.com>
2024-03-21 11:07:06 -0700

    changes to pass test and to have the website function

--- scriptHelper.js
+++ scriptHelper.js
@@ -95,8 +95,8 @@ function pickPlanet(planets) {
 }
 
 
-// module.exports.addDestinationInfo = addDestinationInfo;
-// module.exports.validateInput = validateInput;
-// module.exports.formSubmission = formSubmission;
-// module.exports.pickPlanet = pickPlanet;
-// module.exports.myFetch = myFetch;
+module.exports.addDestinationInfo = addDestinationInfo;
+module.exports.validateInput = validateInput;
+module.exports.formSubmission = formSubmission;
+module.exports.pickPlanet = pickPlanet;
+module.exports.myFetch = myFetch;
